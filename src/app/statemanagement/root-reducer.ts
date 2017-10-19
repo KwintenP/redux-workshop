d7c3abb3be1fd38eadffe956ec62c09af1f19e3a
KwintenP <kwinten.pisman@gmail.com>
2017-10-19 13:20:17 +0200

    Fixed the root reducer.

--- src/app/statemanagement/root-reducer.ts
+++ src/app/statemanagement/root-reducer.ts
@@ -1,11 +1,12 @@
 import {StarWarsCharacter} from '../modules/swapi/entities/star-wars-character.entity';
-import {ActionReducer, ActionReducerMap, combineReducers, MetaReducer} from '@ngrx/store';
+import {ActionReducer, ActionReducerMap, combineReducers, MetaReducer, compose} from '@ngrx/store';
 import {characterReducer} from './data/characters';
 import {environment} from '../../environments/environment';
 import {storeFreeze} from 'ngrx-store-freeze';
 import {loadingReducer} from './ui/loading';
 import {reset} from './metareducers/reset.reducer';
 import {overviewSortingReducer} from './ui/overview-sorting';
+import {InjectionToken} from '@angular/core';
 
 export type ApplicationState = Readonly<{
   data: {
@@ -16,6 +17,8 @@ export type ApplicationState = Readonly<{
     overviewSorting: {columnName?: string, direction?: 'ASC'|'DESC'}
   };
 }>;
+
+export const reducerToken = new InjectionToken<ActionReducerMap<ApplicationState>>('Reducers');
 
 const dataReducers = combineReducers({
   characters: characterReducer
@@ -31,4 +34,15 @@ export const rootReducer: ActionReducerMap<ApplicationState> = {
   ui: uiReducers,
 };
 
+export function getReducers() {
+  return rootReducer;
+}
+
+export const reducerProvider = [
+  { provide: reducerToken, useFactory: getReducers }
+];
+
 export const metaReducers: MetaReducer<ApplicationState>[] = environment.production ? [reset] : [storeFreeze, reset];
+export function getMetaReducers(): MetaReducer<ApplicationState>[] {
+  return metaReducers;
+}
