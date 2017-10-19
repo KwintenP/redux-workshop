a4f5d779cafb55544a4fcc33eeb54be24cbe93ea
KwintenP <kwinten.pisman@gmail.com>
2017-10-19 13:14:43 +0200

    Removed reducer implementations.

--- src/app/statemanagement/ui/loading.spec.ts
+++ src/app/statemanagement/ui/loading.spec.ts
@@ -5,44 +5,25 @@ import {init} from 'protractor/built/launcher';
 describe('reducer: loading reducer', () => {
   describe('case NGRX_INIT', () => {
     it('should return the initial state', () => {
-      const initialState = undefined;
 
-      const result = loadingReducer(initialState, {type: 'NGRX_INIT'} as any);
-
-      expect(result).toBeFalsy();
     });
   });
 
   describe('case UNKNOWN_STATE', () => {
     it('should return the current state', () => {
-      const initialState = true;
-      deepFreeze(initialState);
 
-      const result = loadingReducer(initialState, {type: 'UNKNOWN_STATE'} as any);
-
-      expect(result).toBeTruthy();
     });
   });
 
   describe('case SET_LOADING', () => {
     it('should set the loading to true', () => {
-      const initialState = false;
-      deepFreeze(initialState);
 
-      const result = loadingReducer(initialState, new SetLoading());
-
-      expect(result).toBeTruthy();
     });
   });
 
   describe('case LOADING_DONE', () => {
     it('should set the loading to false', () => {
-      const initialState = true;
-      deepFreeze(initialState);
 
-      const result = loadingReducer(initialState, new LoadingDone());
-
-      expect(result).toBeFalsy();
     });
   });
 });
--- src/app/statemanagement/ui/overview-sorting.spec.ts
+++ src/app/statemanagement/ui/overview-sorting.spec.ts
@@ -5,60 +5,25 @@ import {init} from 'protractor/built/launcher';
 describe('reducer overviewSortingReducer', () => {
   describe('case NGRX_INIT', () => {
     it('should return the initial state', () => {
-      const initialState = undefined;
-
-      const result = overviewSortingReducer(initialState, {type: 'NGRX_INIT'} as any);
-
-      expect(result).toEqual({});
     });
   });
 
   describe('case UNKNOWN_STATE', () => {
     it('should return the current state', () => {
-      const initialState = {columnName: 'name', direction: 'ASC' as 'ASC'};
-      deepFreeze(initialState);
-
-      const result = overviewSortingReducer(initialState, {type: 'UNKNOWN_STATE'} as any);
-
-      expect(result).toBe(initialState);
     });
   });
 
   describe('on SET_SORTING', () => {
-      it('should add the sorting and set the direction to ASC when there is no sorting yet', () => {
-        const initialState = {}
-        deepFreeze(initialState);
-
-        const result = overviewSortingReducer(initialState, new SetSorting({columnName: name}));
-
-        expect(result).toEqual({columnName: name, direction: 'ASC'});
-      });
+    it('should add the sorting and set the direction to ASC when there is no sorting yet', () => {
+    });
 
     it('should add the sorting and set the direction to DESC when the current direction is ASC', () => {
-      const initialState = {columnName: name, direction: 'ASC' as 'ASC'}
-      deepFreeze(initialState);
-
-      const result = overviewSortingReducer(initialState, new SetSorting({columnName: name}));
-
-      expect(result).toEqual({columnName: name, direction: 'DESC'});
     });
 
     it('should add the sorting and set the direction to ASC when the current direction is DESC', () => {
-      const initialState = {columnName: name, direction: 'DESC' as 'DESC'}
-      deepFreeze(initialState);
-
-      const result = overviewSortingReducer(initialState, new SetSorting({columnName: name}));
-
-      expect(result).toEqual({columnName: name, direction: 'ASC'});
     });
 
     it('should override the sorting and set the direction to ASC when its a different columnName', () => {
-      const initialState = {columnName: name, direction: 'DESC' as 'DESC'}
-      deepFreeze(initialState);
-
-      const result = overviewSortingReducer(initialState, new SetSorting({columnName: 'gender'}));
-
-      expect(result).toEqual({columnName: 'gender', direction: 'ASC'});
     });
   });
 });
