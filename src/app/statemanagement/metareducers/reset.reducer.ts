dddc7bb193ee0e83e9f9a5d24ddfaa1b75197dea
KwintenP <kwinten.pisman@gmail.com>
2017-10-21 17:35:12 +0200

    added meta reducer

---
+++ src/app/statemanagement/metareducers/reset.reducer.ts
@@ -0,0 +1,6 @@
+import {ApplicationState} from '../root-reducer';
+import {ActionReducer} from '@ngrx/store';
+
+export function reset(reducer, initialState?: ApplicationState): ActionReducer<ApplicationState> {
+  // TODO implement
+}
